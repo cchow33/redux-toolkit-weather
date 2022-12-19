df91a179a161ff8a39febc20359f4bb46632ac5f
Carmen Chow <carmendevelops@gmail.com>
2022-12-19 15:48:24 -0500

    package.json error

---
+++ src/components/displayWeather.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const displayWeather = () => {
+  return (
+    <div>displayWeather</div>
+  )
+}
+
+export default displayWeather
---
+++ src/components/searchBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const searchBar = () => {
+  return (
+    <div className="searchbar">Enter a city to search for the weather</div>
+  )
+}
+
+export default searchBar
